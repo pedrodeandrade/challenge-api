e1eea2fa3b74c67826ed3ef4baa60ae72a21bdbb
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-04 04:15:39 -0300

    add some routes

--- lib/challenge_api_web/router.ex
+++ lib/challenge_api_web/router.ex
@@ -19,6 +19,19 @@ defmodule ChallengeApiWeb.Router do
     get "/", PageController, :index
   end
 
+  scope "/api", ChallengeApiWeb do 
+    pipe_through :api
+
+    get "/users", UserController, :index
+    get "/users/:id/show", UserController,:show
+    post "/users/create", UserController, :create
+
+
+    post "/users/:u_id/article", ArticleController, :create  
+    get  "/articles", ArticleController, :index
+    
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", ChallengeApiWeb do
   #   pipe_through :api
