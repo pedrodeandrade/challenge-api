7e2c7a1637b1936eb303d62aa01191bb097b887e
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-04 04:18:14 -0300

    add relation between user and article schemas

--- lib/challenge_api/accounts/user.ex
+++ lib/challenge_api/accounts/user.ex
@@ -2,10 +2,12 @@ defmodule ChallengeApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  schema "users" do
+  schema "users" do 
     field :age, :integer
     field :cpf, :string
     field :name, :string
+
+    has_many :articles, ChallengeApi.Submissions.Article
 
     timestamps()
   end
@@ -14,6 +16,8 @@ defmodule ChallengeApi.Accounts.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :cpf, :age])
+    |> validate_length(:cpf,is: 11)
+    |> validate_number(:age, greater_than: 0)
     |> validate_required([:name, :cpf, :age])
   end
 end
--- lib/challenge_api/submissions/article.ex
+++ lib/challenge_api/submissions/article.ex
@@ -4,6 +4,7 @@ defmodule ChallengeApi.Submissions.Article do
   alias ChallengeApi.Accounts.User
 
   schema "articles" do
+    field :title, :string
     field :text, :string
 
     belongs_to :user, User, foreign_key: :user_id
@@ -12,9 +13,13 @@ defmodule ChallengeApi.Submissions.Article do
   end
 
   @doc false
-  def changeset(article, attrs) do
+  def changeset(article, attrs,user_id) do
+    {:ok,user_id} = Ecto.Type.cast(:integer, user_id)
+
     article
-    |> cast(attrs, [:text])
+    |> cast(attrs, [:title,:text,:user_id])
     |> validate_required([:text])
+    |> change(user_id: user_id)
+
   end
 end
