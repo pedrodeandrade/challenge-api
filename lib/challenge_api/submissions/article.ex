e703d8c711b4d3f1fe3271f85a20c64c81427c46
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-05 20:04:34 -0300

    fix changeset

--- lib/challenge_api/submissions/article.ex
+++ lib/challenge_api/submissions/article.ex
@@ -2,12 +2,14 @@ defmodule ChallengeApi.Submissions.Article do
   use Ecto.Schema
   import Ecto.Changeset
   alias ChallengeApi.Accounts.User
+  alias ChallengeApi.Events.Event
 
   schema "articles" do
     field :title, :string
     field :text, :string
 
     belongs_to :user, User, foreign_key: :user_id
+    belongs_to :event, Event, foreign_key: :event_id
 
     timestamps()
   end
@@ -17,8 +19,11 @@ defmodule ChallengeApi.Submissions.Article do
     {:ok, user_id} = Ecto.Type.cast(:integer, user_id)
 
     article
-    |> cast(attrs, [:title, :text, :user_id])
-    |> validate_required([:text])
+    |> cast(attrs, [:title, :text,:user_id, :event_id])
+    |> validate_required([:title, :text, :event_id])
     |> change(user_id: user_id)
   end
 end
+
+
+#SELECT articles.title, articles.text, users.name, events.name FROM articles,users,events where users.id = articles.user_id and articles.event_id = events.id;
