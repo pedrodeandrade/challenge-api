2bf33c87f7896232e30a7637e7075654626a844f
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-10 20:37:47 -0300

    fix view

--- lib/challenge_api_web/views/user_view.ex
+++ lib/challenge_api_web/views/user_view.ex
@@ -7,10 +7,25 @@ defmodule ChallengeApiWeb.UserView do
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, UserView, "user.json")}
+    %{data: render_one(%{user: user}, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user, token: token}) do
+    %{
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: token
+    }
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id, name: user.name, cpf: user.cpf, age: user.age}
+    %{
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      age: user.age,
+      cpf: user.cpf
+    }
   end
 end
