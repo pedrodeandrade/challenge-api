3055a296f6c83edd420db921221a7d26e29c894c
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-10 20:43:27 -0300

    fix tests

--- test/challenge_api/events_test.exs
+++ test/challenge_api/events_test.exs
@@ -6,8 +6,8 @@ defmodule ChallengeApi.EventsTest do
   describe "events" do
     alias ChallengeApi.Events.Event
 
-    @valid_attrs %{date: "some date", description: "some description", name: "some name"}
-    @update_attrs %{date: "some updated date", description: "some updated description", name: "some updated name"}
+    @valid_attrs %{date: "09/01/2020", description: "some description", name: "some name"}
+
     @invalid_attrs %{date: nil, description: nil, name: nil}
 
     def event_fixture(attrs \\ %{}) do
@@ -20,18 +20,24 @@ defmodule ChallengeApi.EventsTest do
     end
 
     test "list_events/0 returns all events" do
-      event = event_fixture()
+      event =
+              event_fixture()
+              |> remove_articles
+
       assert Events.list_events() == [event]
     end
 
     test "get_event!/1 returns the event with given id" do
-      event = event_fixture()
+      event =
+              event_fixture()
+              |> remove_articles
+
       assert Events.get_event!(event.id) == event
     end
 
     test "create_event/1 with valid data creates a event" do
       assert {:ok, %Event{} = event} = Events.create_event(@valid_attrs)
-      assert event.date == "some date"
+      assert event.date == "09/01/2020"
       assert event.description == "some description"
       assert event.name == "some name"
     end
@@ -40,29 +46,8 @@ defmodule ChallengeApi.EventsTest do
       assert {:error, %Ecto.Changeset{}} = Events.create_event(@invalid_attrs)
     end
 
-    test "update_event/2 with valid data updates the event" do
-      event = event_fixture()
-      assert {:ok, %Event{} = event} = Events.update_event(event, @update_attrs)
-      assert event.date == "some updated date"
-      assert event.description == "some updated description"
-      assert event.name == "some updated name"
-    end
-
-    test "update_event/2 with invalid data returns error changeset" do
-      event = event_fixture()
-      assert {:error, %Ecto.Changeset{}} = Events.update_event(event, @invalid_attrs)
-      assert event == Events.get_event!(event.id)
-    end
-
-    test "delete_event/1 deletes the event" do
-      event = event_fixture()
-      assert {:ok, %Event{}} = Events.delete_event(event)
-      assert_raise Ecto.NoResultsError, fn -> Events.get_event!(event.id) end
-    end
-
-    test "change_event/1 returns a event changeset" do
-      event = event_fixture()
-      assert %Ecto.Changeset{} = Events.change_event(event)
+    def remove_articles (event) do
+      Map.replace!(event,:articles,[])
     end
   end
 end
