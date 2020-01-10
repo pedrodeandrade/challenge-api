d873e36720bd8feb8b0eb2ac8a2035e5d05e7691
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-10 20:39:48 -0300

    add email and password validation

--- lib/challenge_api/accounts/user.ex
+++ lib/challenge_api/accounts/user.ex
@@ -1,11 +1,15 @@
 defmodule ChallengeApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import Comeonin.Bcrypt
 
   schema "users" do
     field :age, :integer
     field :cpf, :string
     field :name, :string
+    field :email, :string
+    field :encrypted_password, :string
+    field :password, :string, virtual: true
 
     has_many :articles, ChallengeApi.Submissions.Article
 
@@ -15,9 +19,23 @@ defmodule ChallengeApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :cpf, :age])
+    |> cast(attrs, [:name, :cpf, :age, :email, :password])
     |> validate_length(:cpf, is: 11)
     |> validate_number(:age, greater_than: 0)
-    |> validate_required([:name, :cpf, :age])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> unique_constraint(:email)
+    |> validate_required([:name, :cpf, :age, :email, :password])
+    |> put_hashing_pw
+  end
+
+  defp put_hashing_pw(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :encrypted_password, hashpwsalt(password))
+
+      _ ->
+        changeset
+    end
   end
 end
