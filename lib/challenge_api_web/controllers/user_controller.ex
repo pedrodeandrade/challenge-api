f4c3ee2030b68bea3c808331edd019d807786640
Pedro-AndradeS <pedro9446@gmail.com>
2020-01-10 21:45:03 -0300

    delete useless function

--- lib/challenge_api_web/controllers/user_controller.ex
+++ lib/challenge_api_web/controllers/user_controller.ex
@@ -3,40 +3,25 @@ defmodule ChallengeApiWeb.UserController do
 
   alias ChallengeApi.Accounts
   alias ChallengeApi.Accounts.User
+  alias ChallengeApi.Guardian
 
   action_fallback ChallengeApiWeb.FallbackController
 
-  def index(conn, _params) do
-    users = Accounts.list_users()
-    render(conn, "index.json", users: users)
-  end
-
+  # request example = {user: {"name": "roberto", "age": "10", "cpf": "03457188231, "email": "roberto@gmail.com}, "password": "12345678"}
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
+    with {:ok, %User{} = user} <- Accounts.create_user(user_params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
       conn
       |> put_status(:created)
-      |> render("show.json", user: user)
+      |> render("user.json", %{user: user, token: token})
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, "show.json", user: user)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
-
-    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
-      render(conn, "show.json", user: user)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-
-    with {:ok, %User{}} <- Accounts.delete_user(user) do
-      send_resp(conn, :no_content, "")
+  # request example = {"email": "roberto@gmail.com", "password" "12345678"}
+  def sign_in(conn, %{"email" => email, "password" => password}) do
+    with {:ok, user, token} <- Accounts.sign_in_token(email, password) do
+      conn
+      |> render("user.json", %{user: user, token: token})
     end
   end
 end
